ccd7a4acea39a12a3d1aae9cceddc4a6b98b967b
QFPayShadowMan <qfpayshadowman@pn-206-196.itsc.cuhk.edu.hk>
2016-02-03 18:47:01 +0800

    start version
    version 1.0.0

---
+++ XNQLaunchView/XNQLaunchView/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  XNQLaunchView
+//
+//  Created by Semyon on 16/2/3.
+//  Copyright © 2016年 xnq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
+
---
+++ XNQLaunchView/XNQLaunchView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XNQLaunchView
+//
+//  Created by Semyon on 16/2/3.
+//  Copyright © 2016年 xnq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
